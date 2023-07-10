fd0ea7971a99e42700f49b49ef35f6256cad7402
teruteru <tutituti3080@gmail.com>
2023-07-10 09:30:44 +0900

    post_method

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [name, setName] = useState(""); // 新規ユーザ名を保持するステート
 
   useEffect(() => {
     const getUsers = async () => {
@@ -19,9 +20,29 @@ const App = () => {
     getUsers();
   }, []);
 
+  const handleChange = (e) => {
+    setName(e.target.value); // 入力値をnameステートに保存
+  };
+
+  const createNewUser = () => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", {
+        name: name, // nameステートの値をPOST
+      })
+      .then((response) => {
+        setUsers([...users, response.data]); // 新規ユーザをusersステートに追加
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>Hello React</h1>
+      <input value={name} onChange={handleChange} />
+      <br />
+      <button onClick={createNewUser}>作成</button>
       <ul>
         {users.map((user, index) => (
           <li key={index}>{user.name}</li> // データを表示
