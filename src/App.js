8de64a14d1e2f68693779b1e9b7a406ed9303dc7
teruteru <tutituti3080@gmail.com>
2023-07-10 09:48:44 +0900

    put_method

--- src/App.js
+++ src/App.js
@@ -37,6 +37,33 @@ const App = () => {
       });
   };
 
+  const deleteUser = (id) => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => {
+        setUsers(users.filter((user) => user.id !== id)); // IDに一致しないユーザーだけを残すことでユーザーを削除
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const modifyUser = (id) => {
+    axios
+      .patch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+        name: "John Doe",
+      })
+      .then((response) => {
+        let updatedUsers = users.map((user) => {
+          if (user.id === response.data.id) {
+            return response.data;
+          } else {
+            return user;
+          }
+        });
+        setUsers(updatedUsers); // ユーザー情報を更新
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
       <h1>Hello React</h1>
@@ -45,7 +72,13 @@ const App = () => {
       <button onClick={createNewUser}>作成</button>
       <ul>
         {users.map((user, index) => (
-          <li key={index}>{user.name}</li> // データを表示
+          <li key={index}>
+            {user.name}
+            <button onClick={() => deleteUser(user.id)}>削除</button> //
+            ユーザーごとに削除ボタンを追加
+            <button onClick={() => modifyUser(user.id)}>更新</button> //
+            ユーザーごとに更新ボタンを追加
+          </li>
         ))}
       </ul>
     </div>
